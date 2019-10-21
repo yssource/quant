b0fb45924be7ea09c999bf6a290fbad7b34f4753
xyhuang <huangxy17@fudan.edu.cn>
2019-10-21 16:18:10 +0800

    debug for pt_report script

--- scripts/PT_report.sh
+++ scripts/PT_report.sh
@@ -1,4 +1,5 @@
-#!/bin/zsh
+#!/bin/bash
 export LD_LIBRARY_PATH=/usr/local/lib
+export PYTHONPATH=/root/quant/tools:/root/lib-hft/lib/cpp_py:/root/quant/tools/common
 
-/root/anaconda2/bin/python /root/quant/report/PT_report.py > /root/pt_report.log
+/root/anaconda2/bin/python /root/quant/report/PT_report.py > /root/PT_report.log  2>&1
